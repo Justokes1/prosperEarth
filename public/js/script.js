ae8b718a97f13b1a61236b9655b4966b16edacfc
Justokes1 <102987700+Justokes1@users.noreply.github.com>
2022-07-06 02:26:55 -0400

    troubleshooting materialize

--- public/js/script.js
+++ public/js/script.js
@@ -1,12 +1,12 @@
 // SCRIPT FOR NAVBAR
 document.addEventListener("DOMContentLoaded", function () {
   var elems = document.querySelectorAll(".sidenav");
-  var instances = M.Sidenav.init(elems, options);
+  var instances = M.Sidenav.init(elems, {});
 });
 
 // SCRIPT FOR FAQ
 
 document.addEventListener("DOMContentLoaded", function () {
   var elems = document.querySelectorAll(".collapsible");
-  var instances = M.Collapsible.init(elems, options);
+  var instances = M.Collapsible.init(elems, {});
 });
