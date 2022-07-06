7f5e4c9b604468ff7982212a924f1d77cb6cce2c
Justokes1 <102987700+Justokes1@users.noreply.github.com>
2022-07-06 01:04:27 -0400

    submitting revamped contact form

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,12 @@
+// SCRIPT FOR NAVBAR
 document.addEventListener("DOMContentLoaded", function () {
   var elems = document.querySelectorAll(".sidenav");
   var instances = M.Sidenav.init(elems, options);
+});
+
+// SCRIPT FOR FAQ
+
+document.addEventListener("DOMContentLoaded", function () {
+  var elems = document.querySelectorAll(".collapsible");
+  var instances = M.Collapsible.init(elems, options);
 });
