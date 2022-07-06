2b806aa18a1c9816f7d2773bd4f1845d5885fff1
Sammie Steohenson <sammiestephenson@MacBook-Pro.attlocal.net>
2022-07-05 23:12:58 -0400

    added dotenv and axios. copied some sql coded commands

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
+//S addition setting up connection to Database
+const bodyParser = require("body-parser");
+const dotenv = require('dotenv');
+const router = express.Router();
+const mysql = require('mysql');
 
 //USE STATIC FILES
 app.use(express.static("public"));
@@ -30,3 +35,30 @@ app.get("/faq", (req, res) => {
 
 // LISTEN ON PORT 3000
 app.listen(port, () => console.log(`listening on port ${port}`));
+
+
+//Sammies copy and Paste start 
+
+// Storing contact input in contact table
+const userDetails = req.body;
+
+// Insert data into Contact table
+var sql = 'INSERT INTO hope.contact SET ?';
+db.query(sql, userDetails, function (err, data) {
+  if (err) throw err;
+  console.log("Contact data is inserted successfully ");
+});
+
+// redirect to user form page after inserting the data
+res.redirect('/contact/form');
+
+module.exports = router;
+
+//Retrieve data from contact input/table and see in console log
+app.get("/", (req, res) => {
+  connection.query('SELECT * FROM hope.contact LIMIT 1', (err, rows) => {
+    if (err) throw err;
+    console.log('The data from users table are: \n', rows);
+    connection.end();
+  });
+});
